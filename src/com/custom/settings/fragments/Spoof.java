b9b12c4f674914f3fddde64340d0fe9eabdcec07
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-11 14:52:21 +0800

    spoof: add support for external keybox
    Change-Id: I6dd3ac9996e0bfa2f275f3723b31ceb424b2078e

--- src/com/custom/settings/fragments/Spoof.java
+++ src/com/custom/settings/fragments/Spoof.java
@@ -19,7 +19,10 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.preference.Preference;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -29,6 +32,10 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 @SearchIndexable
@@ -36,8 +43,21 @@ public class Spoof extends SettingsPreferenceFragment implements Preference.OnPr
 
     public static final String TAG = "Spoof";
     private static final String SYS_GPHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private static final String KEY_IMPORT_KEYBOX = "import_keybox";
+    private static final String KEY_CLEAR_KEYBOX = "clear_keybox";
+    private static final String KEYBOX_PATH = "/data/misc/keybox/keybox.xml";
 
     private Preference mGphotosSpoof;
+    private Preference mImportKeybox;
+    private Preference mClearKeybox;
+
+    private final ActivityResultLauncher<String> mImportKeyboxLauncher = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            uri -> {
+                if (uri != null) {
+                    handleKeyboxImport(uri);
+                }
+            });
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +66,18 @@ public class Spoof extends SettingsPreferenceFragment implements Preference.OnPr
 
         mGphotosSpoof = findPreference(SYS_GPHOTOS_SPOOF);
         mGphotosSpoof.setOnPreferenceChangeListener(this);
+
+        mClearKeybox = findPreference(KEY_CLEAR_KEYBOX);
+        mClearKeybox.setOnPreferenceClickListener(preference -> {
+            clearKeybox();
+            return true;
+        });
+
+        mImportKeybox = findPreference(KEY_IMPORT_KEYBOX);
+        mImportKeybox.setOnPreferenceClickListener(preference -> {
+            mImportKeyboxLauncher.launch("text/xml");
+            return true;
+        });
     }
 
     @Override
@@ -60,6 +92,56 @@ public class Spoof extends SettingsPreferenceFragment implements Preference.OnPr
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.VIEW_UNKNOWN;
+    }
+
+    private void handleKeyboxImport(Uri uri) {
+        try (InputStream in = requireContext().getContentResolver().openInputStream(uri);
+             OutputStream out = new FileOutputStream(KEYBOX_PATH)) {
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            setPermissions(KEYBOX_PATH);
+            showToast(R.string.import_success);
+            SystemRestartUtils.showSystemRestartDialog(getContext());
+        } catch (Exception e) {
+            Log.e(TAG, "Keybox import failed", e);
+            showToast(R.string.import_failed);
+        }
+    }
+
+    private void setPermissions(String path) {
+        try {
+            File file = new File(path);
+            file.setReadable(false, false);
+            file.setWritable(false, false);
+            file.setReadable(true, true);
+            file.setWritable(true, true);
+        } catch (Exception e) {
+            Log.e(TAG, "Permission set failed", e);
+        }
+    }
+
+    private void showToast(int resId) {
+        getActivity().runOnUiThread(() -> 
+            Toast.makeText(getContext(), resId, Toast.LENGTH_SHORT).show()
+        );
+    }
+
+    private void clearKeybox() {
+        try {
+            File file = new File(KEYBOX_PATH);
+            if (file.exists() && file.delete()) {
+                showToast(R.string.clear_success);
+                SystemRestartUtils.showSystemRestartDialog(getContext());
+            } else {
+                showToast(R.string.clear_failed);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to clear keybox", e);
+            showToast(R.string.clear_failed);
+        }
     }
 
     /**
